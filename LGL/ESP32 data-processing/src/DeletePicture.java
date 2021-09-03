b2308eaa8ae093c09f50bde54c7d3657a6d3a674
3019244337-Liu Gelun <72080158+Caffrey-Liu@users.noreply.github.com>
2021-09-03 12:34:04 +0800

    9.3

--- LGL/ESP32 data-processing/src/DeletePicture.java
+++ LGL/ESP32 data-processing/src/DeletePicture.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class DeletePicture {
     static String Path1 = "D:\\GITHUB\\333-334\\LGL\\ESP32 data-processing\\src\\jpg\\";
-    static String Path2 = "D:\\GITHUB\\333-334\\LGL\\ESP32 data-processing\\out\\production\\ESP32 data-processing\\jpg";
+    static String Path2 = "D:\\GITHUB\\333-334\\LGL\\ESP32 data-processing\\out\\production\\ESP32 data-processing\\jpg\\";
 
     public static void folderMethod(String path) {
         File file = new File(path);
